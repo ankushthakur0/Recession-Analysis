6ad7963af387a7fb85f6cc6696d26efd8ee15d4f
ankushthakur0 <ankushrajput1918@gmail.com>
2024-01-09 21:31:55 -0500

    plotting

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data = pd.read_csv('Metadata_Country_API_NY.GDP.MKTP.CD_DS2_en_csv_v2_6298258.csv')
-print("*****")
-print(data.head())
+data = pd.read_csv('Metadata_Country_API_NY.GDP.MKTP.CD_DS2_en_csv_v2_6298258.csv', index_col=0)
+
+data["Duration"].plot(kind = 'hist')
+
+plt.show()
+
+# print("*****")
+# print(data.head())
+
+
+
+# data = data.loc[data.columns.str.contains('Unnamed')]
+# print(data)
 
